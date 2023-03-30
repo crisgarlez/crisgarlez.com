9343445d22fa900257f0a5c011ee06eeb175cfa4
Cristhian Garcia Vélez <escipion25@gmail.com>
2023-03-30 13:18:37 -0500

    Change website name

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'Cristhian García Vélez',
   author: 'Tails Azimuth',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
